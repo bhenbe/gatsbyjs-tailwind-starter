1ff37d4823e44a89961c9e6ca0f2375068f29f98
bhenbe <contact@bhen.be>
2020-04-05 22:06:01 +0200

    add component

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,13 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Card from "../components/card"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Card></Card>
+    <Link className="btn" to="/">Go back to the homepage</Link>
   </Layout>
 )
 
